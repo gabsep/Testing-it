5e802c0ad698243011fe664830e786bb692589d3
GABRIELPC\gab <gabsep5@gmail.com>
2017-12-14 22:55:57 -0800

    Added another print statement

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,6 +7,7 @@ public class Test : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("This is an awesome test");
+        print("something else");
 	}
 	
 	// Update is called once per frame
